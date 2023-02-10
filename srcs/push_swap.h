4397768ab884107b6a1bd56c05d605eae5b4325c
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-02-09 23:29:49 -0300

    FEAT: Create function find_max_binary_positio This function return the number of digits from a binary number

--- srcs/push_swap.h
+++ srcs/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dapaulin <dapaulin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 18:24:20 by dapaulin          #+#    #+#             */
-/*   Updated: 2023/02/09 22:33:51 by dapaulin         ###   ########.fr       */
+/*   Updated: 2023/02/09 23:01:00 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ void		clear_stack(t_stack **stack, void (*del)(void *));
 // Sort functions
 int			find_max_num(t_stack *stack);
 void    	radix_sort(t_stack *stack);
+int 		find_max_binary_position(int max_num);
 
 // Actions
 void    	rotate_a(t_stack *a);
--- srcs/radix_sort.c
+++ srcs/radix_sort.c
@@ -6,7 +6,7 @@
 /*   By: dapaulin <dapaulin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 22:11:26 by dapaulin          #+#    #+#             */
-/*   Updated: 2023/02/09 22:44:09 by dapaulin         ###   ########.fr       */
+/*   Updated: 2023/02/09 23:24:49 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,25 @@ int     find_max_num(t_stack *stack)
     return (max_num);
 }
 
+int find_max_binary_position(int max_num)
+{
+    int i;
+
+    i = 0;
+    if (max_num < 0)
+        return (0);
+    while (max_num != 0)
+    {
+        i++;
+        max_num = max_num >> 1;
+    }
+    return (i);
+}
+
 void    radix_sort(t_stack *stack)
 {
-    find_max_num(stack);
+    int i;
+    
+    i = find_max_binary_position(find_max_num(stack));
+    
 }
